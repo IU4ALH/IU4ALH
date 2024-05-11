640a37371948a50d524810b806ca9eb6ca653dec
Antonino <100965241+IU4ALH@users.noreply.github.com>
2024-05-11 19:45:41 +0200

    Add files via upload

---
+++ VFO_ESP32_JF3HZB_and_IU4ALH_Mods/VFO_ESP32_2_Pulsanti/vfo_esp32_2pulsanti/dial.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   dial.h
+ * Author: JF3HZB / T.UEBO
+ *
+ * Created on 2019/02/10, 22:13
+ */
+
+
+#ifndef DIAL_H
+#define	DIAL_H
+
+void init_Dial(void);
+void Dial(long freq);
+void dot(float, float);
+
+void Sel_font12(void);
+void Sel_font14(void);
+void Sel_font16(void);
+
+#endif	/* DIAL_H */
